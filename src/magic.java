c43bde1fbf089d2bb9417dd0ebb3dbfa4842cfe5
LanluZ <2749792009@qq.com>
2021-02-28 10:36:22 +0800

    Finished version(Maybe)

--- src/magic.java
+++ src/magic.java
@@ -3,36 +3,38 @@ import java.util.Arrays;
 class magic {
     public static void rule(int a[]) {
         /*
+        //bug检查
         for(int i = 0; i < 9; i++)
             System.out.print(a[i]);
         System.out.println();
         */
 
         for (int i = 0; i < 3; i++) {
-            if (((a[0 + i] + a[1 + i] + a[2 + i]) != 15) ||
-                    ((a[0 + i] + a[3 + i] + a[6 + i]) != 15) ||
-                    ((a[0] + a[4] + a[8] != 15)) ||
-                    ((a[0] + a[1] + a[2]) != 15))
+            if (a[0 + i * 3] + a[1 + i * 3] + a[2 + i * 3] != 15 ||
+                    a[0 + i] + a[3 + i] + a[6 + i] != 15 ||
+                    a[0] + a[4] + a[8] != 15 ||
+                    a[0] + a[1] + a[2] != 15)
                 return;
         }
-        boolean con = false;
+        boolean con = true;
         for (int i = 0, j = 0; i < 9; j++) {
-            if (j == 9) {
+            if (j >= 9) {
                 j = 0;
                 i++;
             }
-            if (i == j)
+            if (i == j || i == 9)
                 continue;
             if (a[i] == a[j]) {
-                con = true;
+                con = false;
                 break;
             }
         }
-        if (con == false) {
-            System.out.println("[");
+        //System.out.print(con);
+        if (con == true) {
+            System.out.println("-----");
             for (int i = 0; i < 9; i++) {
                 System.out.print(a[i] + ",");
-                if ((i % 3) == 0)
+                if ((i % 3) == 2)
                     System.out.println();
             }
         }
@@ -40,16 +42,13 @@ class magic {
     }
 
     public static void sort(int a[], int step) {
-        //System.out.print(step);
         for (int i = 1; i <= 9; i++) {
             a[step] = i;
             if (step == 8) {
                 rule(a);
-                return;
             }
             if (step < 8) {
                 sort(a, (step + 1));
-                //System.out.print("["+step+"]");
                 continue;
             }
         }
